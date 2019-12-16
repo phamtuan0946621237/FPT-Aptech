872c39900beef269db6ec4f686f20ba1310107f0
phamtuan <phamtuannd200997@gmail.com>
2019-12-16 17:53:22 +0700

    tuan

---
+++ ag6.cpp
@@ -0,0 +1,138 @@
+
+
+#include <stdio.h>
+
+int main() {
+    
+//    int ary[3];
+//    printf("nhập dãy\n");
+//    for(int i=0;i<3;i++){
+//
+//        scanf("%d",&ary[i]);
+//    }
+//    int s=0;
+//    float t;
+//    for (int i=0;i<3;i++){
+//        s+= ary[i];
+//        printf("%d\n",s);
+//        t = s/3;
+//        printf("%f",t);
+//    }
+    
+//    1. Tìm số gần lớn nhất, gần nhỏ nhất trong mảng n số
+
+//    int day[5];
+//        printf("nhập dãy\n");
+//        for(int i=0;i<5;i++){
+//
+//            scanf("%d",&day[i]);
+//        }
+//    int max=day[0];
+//    int max2=day[0];
+//    for(int i=1;i<5;i++){
+//        if(max< day[i]){
+//            max=day[i];
+//
+//        }
+//    }
+//    for(int i=1;i<5;i++){
+//        if(max2 < day[i] && day[i] < max ){
+//            max2 = day[i];
+//        }
+//    }
+//    if(max=day[0]){
+//        max2 <max;
+//        printf("%d",max2);
+//    }else{
+//    printf("%d là số lớn thứ 2 trong dãy\n",max2);
+//    }
+////
+//    2. Nhập vào 1 mảng n số, in ra các số lẻ trong mảng
+
+//            int a[5];
+//            printf("nhập dãy\n");
+//            for(int i=0;i<5;i++){
+//
+//                scanf("%d",&a[i]);
+//            }
+//    for(int i=0;i<5;i++){
+//        if(a[i]%2 == 1){
+//            printf("%d\n",a[i]);
+//        }
+//    }
+//    1. Tính trung bình cộng các số lẻ trong mảng n số
+//    int a[5];
+//    int tong=0;
+//    float tb;
+//    printf("nhập dãy \n");
+//    for(int i=0;i<5;i++){
+//        scanf("%d",&a[i]);
+//    }
+//    for(int i=0 ; i<5 ; i++){
+//        if(a[i]%2==1){
+//            tong+= a[i];
+//            tb=tong/2;
+////            printf("%f\n",tb);
+////            printf("%d",a[i]);
+//            printf("%f\n",tb);
+//        }
+//    }
+//    2. Tính trung bình cộng các số lẻ ở vị trí chẵn trong mảng n số
+//    int n;
+//    printf("nhập số n");
+//    scanf("%d",&n);
+//    int a[n];
+//       int tong=0;
+//       float tb;
+//       printf("nhập dãy \n");
+//       for(int i=0;i<n;i++){
+//           scanf("%d",&a[i]);
+//       }
+//    for(int i=1;i < n;i+=2){
+//        if(a[i]%2==1){
+//        tong+= a[i];
+//        printf("%d\n",tong);
+//        }else{
+//            printf("không có số lẻ nào ở vị trí chẵn và tổng bằng 0");
+//        }
+//    }
+////    3. Thay thế các phần tử âm có trong mảng bằng giá trị 0
+//    int n;
+//        printf("nhập số n");
+//        scanf("%d",&n);
+//        int a[n];
+//
+//           printf("nhập dãy \n");
+//           for(int i=0;i<n;i++){
+//               scanf("%d",&a[i]);
+//           }
+//    for(int i=0;i<n;i++){
+//        if(a[i]%2==1){
+//            a[i]=0;
+//
+//        }
+//        printf("%d\n",a[i]);
+//    }
+//    4. Nhập 1 mảng n phần tử với điều kiện không được nhập trùng số
+//    int n;
+//            printf("nhập số n");
+//            scanf("%d",&n);
+//            int a[n];
+//            int b=a[0];
+//               printf("nhập dãy \n");
+//               for(int i=0;i<n;i++){
+//                   scanf("%d",&a[i]);
+//               }
+//    for(int i=0;i<n;i++){
+//        for( int j=i+1;j<n;j++){
+//            if(a[i]==a[j]){
+//                printf("nhập lại\n");
+//            }else{
+//                printf("nhập đúng\n");
+//            }
+//        }
+//    }
+//    5. In ra các số nghịch đảo của các số trong mảng 10 số.
+        
+}
+
